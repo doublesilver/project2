90336307f849488c30a7c6bd19e66a1cb5093811
doublesilver <doublesilver@users.noreply.github.com>
2026-02-20 16:25:49 +0900

    chore: remove all repository contents

--- job_scraper/job_scraper/items.py
+++
@@ -1,8 +0,0 @@
-import scrapy
-
-class JobItem(scrapy.Item):
-    url = scrapy.Field()
-    title = scrapy.Field()
-    company = scrapy.Field()
-    location = scrapy.Field()
-    description = scrapy.Field()
